353066d8412edfdeeab026b5c5326d751d585de3
Cooper Lawson <CSLawson@lcs.on.ca>
2023-01-25 11:18:29 -0500

    Initial Commit

---
+++ StucturesRecap/StucturesRecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  StucturesRecapApp.swift
+//  StucturesRecap
+//
+//  Created by Cooper Lawson on 2023-01-25.
+//
+
+import SwiftUI
+
+@main
+struct StucturesRecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
